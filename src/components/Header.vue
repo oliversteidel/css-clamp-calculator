a76568a9ab7270b2b0593c95cef5623fc934367d
Oliver Steidel <oliversteidel@yahoo.de>
2022-01-02 20:01:33 +0100

    setup

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header">
+    <h1>Clamp Calculator</h1>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+@use "../scss/setup/index" as *;
+
+.header {
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 5rem;
+  padding: 1rem 3rem;
+  background: $clr-light-grey;
+  box-shadow: $clr-shadow;
+
+  h1 {
+    font-weight: 400;
+  }
+}
+</style>
